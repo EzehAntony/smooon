77600f12cda34231717e088e6c53b58a68aaa49c
Anthony <58951485+EzehAntony@users.noreply.github.com>
2022-09-29 09:56:03 +0100

    home and card fix

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,21 +1,25 @@
 import style from "../styles/Card.module.css";
+import TinderCard from "react-tinder-card";
 
-function Card() {
+function Card({ name, state, age }) {
   return (
-    <div className={style.card}>
-      <div className={style.up}>
-        <img src="/henessy.jpg" alt="" />
-        <div className={style.action}>
-          <img src="/rheart.svg" alt="" />
-          <img src="/star.svg" alt="" />
-          <img src="/close.svg" alt="" />
+    <TinderCard>
+      <div className={style.card}>
+        <div className={style.up}>
+          <img src="/henessy.jpg" alt="" />
+          <div className={style.action}>
+            <img src="/rheart.svg" alt="" />
+            <img src="/star.svg" alt="" />
+            <img src="/close.svg" alt="" />
+          </div>
+        </div>
+        <div className={style.down}>
+          <h3 className="name">{name}</h3>
+          <p className="location">{state}</p>
+          <h5 className="age">{age}</h5>
         </div>
       </div>
-      <div className={style.down}>
-        <h3 className="name">Tatiana Saris</h3>
-        <p className="location">Florida, USA </p>
-        <h5 className="age">21</h5>
-      </div>    </div>
+    </TinderCard>
   );
 }
 
--- pages/home.jsx
+++ pages/home.jsx
@@ -5,6 +5,17 @@ import style from "../styles/home.module.css";
 import Image from "next/image";
 
 function Home() {
+  const data = [
+    { name: "Avinci Morgan", state: "Oklahoma", age: 24 },
+    {
+      name: "Chris Yager",
+      state: "Lagos",
+      age: 42,
+    },
+    { name: "Avinci Eren", state: "San Fransisco", age: 30 },
+    { name: "Mariana Santos", state: "Peru", age: 22 },
+    { name: "Jennifer Mage", state: "Manchester", age: 30 },
+  ];
   return (
     <Layout>
       <div className={style.homePage}>
@@ -15,7 +26,15 @@ function Home() {
 
         {/* Main div that holds the cards */}
         <div className={style.main}>
-          <Card />
+          {data.reverse().map((d, k) => (
+            <Card
+              key={k}
+              name={d.name}
+              state={d.state}
+              age={d.age}
+              styles={d.styles}
+            />
+          ))}
         </div>
       </div>
     </Layout>
